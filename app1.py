36da12adaa2433c6ddc176cb56ab7b4c395d8b56
s-mantha <maggi.mantha@gmail.com>
2019-03-03 23:48:08 +0000

    Update app1.py
    more options

--- app1.py
+++ app1.py
@@ -25,6 +25,7 @@ def generate_chart(): #function to generate a bar plot
     startyear = int(request.form["startyear"])
     endyear = int(request.form["endyear"])   
     disease = request.form["disease"]
+    charttype = request.form.get("charttype")
     if startyear >= endyear :
       return render_template("error.html") 
     else:
@@ -41,18 +42,41 @@ def generate_chart(): #function to generate a bar plot
         lists = sorted(mydict.items()) # sorted by key, return a list of tuples
 
         x, y = zip(*lists) # unpack a list of pairs into two tuples
+        if all([ v == 0 for v in y ]) :# checking if the term doesnt exist.
+            return render_template("nil.html") 
     #prepare the output file
     output_file("Line.html")
 
     p = figure(plot_width=400, plot_height=400,title="PubMed citations")
+    p.xaxis.axis_label = 'Years'
+    p.yaxis.axis_label = 'Disease/disease prone area'
+# BAR PLOT
+    if charttype == "barplot":
+        p.vbar(x=x, top=y,width=0.5)
+        p.xgrid.grid_line_color = None
+        p.y_range.start = 0
+        p.xaxis.axis_label = 'Years'
+        p.yaxis.axis_label = 'Disease'
+        #show(p)
+        html = file_html(p, CDN, "my plot")
+        return html
+# lINE PLOT
+      
+    elif charttype == "line":
+        for_xaxis = [i for i in x]
+        for_yaxis = [j for j in y]
+        p.line(for_xaxis, for_yaxis, line_width=2)
+        html = file_html(p, CDN, "my plot")
+        return html
+#SCATTER PLOT
+    else:
+        for_xaxis = [i for i in x]
+        for_yaxis = [j for j in y]
+        p.circle(for_xaxis,for_yaxis, size=20, color="navy", alpha=0.5)
+        html = file_html(p, CDN, "my plot")
+        return html
 
-# add a line renderer
-    p.vbar(x=x, top=y,width=0.5)
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
-    #show(p)
-    html = file_html(p, CDN, "my plot")
-    return html
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
